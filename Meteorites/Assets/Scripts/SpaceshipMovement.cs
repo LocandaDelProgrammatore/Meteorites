26ad91c5056238aa880fedcc75704cd2b4f11f1a
francescoautera <francesco.autera@gmail.com>
2023-01-18 22:50:01 +0100

    add movement spaceship

---
+++ Meteorites/Assets/Scripts/SpaceshipMovement.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour {
+
+    public float speed;
+    [SerializeField] GameObject modelObject;
+    private Camera camera;
+
+    private void Awake() {
+	    //modelObject = GetComponentInChildren<GameObject>();
+	    camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if (Input.GetKey(KeyCode.W)) {
+		    transform.position += transform.up * speed * Time.deltaTime;
+	    }
+
+	    if (Input.GetKey(KeyCode.S)) {
+		    transform.position += -transform.up * speed * Time.deltaTime;
+	    }
+	    
+	    if (Input.GetKey(KeyCode.A)) {
+		    transform.position += -transform.right * speed * Time.deltaTime;
+	    }
+
+	    if (Input.GetKey(KeyCode.D)) {
+		    transform.position += transform.right * speed * Time.deltaTime;
+	    }
+	    
+	    Vector3 diff = camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+	    diff.Normalize();
+ 
+	    float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+	    modelObject.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+	    
+	    
+    }
+
+    
+
+   
+
+    
+    
+
+}
